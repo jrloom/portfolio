299f13d69dc9e885eb5691293447f8f290b0a740
jason loomis <jason@jrloomis.com>
2020-09-07 03:06:08 -0400

    placeholder pages

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const About = () => (
+  <Layout>
+    <div>About Page</div>
+  </Layout>
+)
+
+export default About
---
+++ src/pages/contact.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const Contact = () => (
+  <Layout>
+    <div>Contact Page</div>
+  </Layout>
+)
+
+export default Contact
