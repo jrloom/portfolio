e509b055b4befb4571d781de21ab93d498701340
jason loomis <jason@jrloomis.com>
2020-09-07 03:24:09 -0400

    created icon file

---
+++ src/components/icons.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { FaCss3Alt, FaHtml5, FaGitAlt, FaNodeJs, FaReact } from 'react-icons/fa'
+import { SiJavascript, SiGraphql } from 'react-icons/si'
+import { AiFillApi } from 'react-icons/ai'
+
+export const TechIcons = () => (
+  <>
+    <FaHtml5 />
+    <FaCss3Alt />
+    <SiJavascript />
+    <FaReact />
+    <FaNodeJs />
+    <AiFillApi />
+    <SiGraphql />
+    <FaGitAlt />
+  </>
+)
