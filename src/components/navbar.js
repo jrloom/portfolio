2eab64e6b0d9776eeec5b02a4b025b04d3743088
jason loomis <jason@jrloomis.com>
2020-09-09 14:46:15 -0400

    added link to github

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import { Icons } from './icons'
 
-const NavBar = ({ links: { home, about, projects, contact } }) => {
-  return (
-    <>
-      <Link className='text-link' to={projects}>
-        Projects
-      </Link>
-      <Link className='text-link' to={contact}>
-        Contact
-      </Link>
-    </>
-  )
-}
+const NavBar = ({ links: { home, about, projects, contact } }) => (
+  <>
+    <Link to={projects} className='text-link icon'>
+      <Icons icons={{ Projects: 'FaCogs' }} />
+    </Link>
+    <Link to={contact} className='text-link icon'>
+      <Icons icons={{ Contact: 'IoMdText' }} />
+    </Link>
+    <a href='https://github.com/jrloom' className='text-link icon'>
+      <Icons icons={{ GitHub: 'SiGithub' }} />
+    </a>
+  </>
+)
 
 export default NavBar
