d0a86a1db3ee3466475fe897a23aae1dff67734f
jason loomis <jason@jrloomis.com>
2020-09-14 06:56:47 -0400

    initial project carousel

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,9 +1,33 @@
 import React from 'react'
+import { Carousel } from 'react-bootstrap'
+import img01 from '../images/placeholder.jpg'
+import img02 from '../images/placeholder02.jpg'
+import img03 from '../images/placeholder03.jpg'
 
 const Projects = () => (
-  <>
-    <div>Projects Page</div>
-  </>
+  <Carousel interval='10000'>
+    <Carousel.Item>
+      <img className='d-block w-100' src={img01} alt='First slide' />
+      <Carousel.Caption>
+        <h3>First slide</h3>
+        <p></p>
+      </Carousel.Caption>
+    </Carousel.Item>
+    <Carousel.Item>
+      <img className='d-block w-100' src={img02} alt='First slide' />
+      <Carousel.Caption>
+        <h3>First slide</h3>
+        <p></p>
+      </Carousel.Caption>
+    </Carousel.Item>
+    <Carousel.Item>
+      <img className='d-block w-100' src={img03} alt='First slide' />
+      <Carousel.Caption>
+        <h3>First slide</h3>
+        <p></p>
+      </Carousel.Caption>
+    </Carousel.Item>
+  </Carousel>
 )
 
 export default Projects
