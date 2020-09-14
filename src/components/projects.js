de5cd0a7f7afb30446a743ee607389015d1fcc08
jason loomis <jason@jrloomis.com>
2020-09-14 12:45:53 -0400

    carousel basic layout

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,33 +1,85 @@
 import React from 'react'
-import { Carousel } from 'react-bootstrap'
-import img01 from '../images/placeholder.jpg'
-import img02 from '../images/placeholder02.jpg'
-import img03 from '../images/placeholder03.jpg'
+import { graphql, useStaticQuery } from 'gatsby'
+import Image from 'gatsby-image'
+import { Carousel, Col, Container, Row } from 'react-bootstrap'
+import { RiExternalLinkLine } from 'react-icons/ri'
+import { Icons } from './icons'
 
-const Projects = () => (
-  <Carousel interval='10000'>
-    <Carousel.Item>
-      <img className='d-block w-100' src={img01} alt='First slide' />
-      <Carousel.Caption>
-        <h3>First slide</h3>
-        <p></p>
-      </Carousel.Caption>
-    </Carousel.Item>
-    <Carousel.Item>
-      <img className='d-block w-100' src={img02} alt='First slide' />
-      <Carousel.Caption>
-        <h3>First slide</h3>
-        <p></p>
-      </Carousel.Caption>
-    </Carousel.Item>
-    <Carousel.Item>
-      <img className='d-block w-100' src={img03} alt='First slide' />
-      <Carousel.Caption>
-        <h3>First slide</h3>
-        <p></p>
-      </Carousel.Caption>
-    </Carousel.Item>
-  </Carousel>
-)
+const Projects = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      projects: allProjectsJson {
+        nodes {
+          id
+          image {
+            url {
+              childImageSharp {
+                fluid {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+          name
+          tech
+          text {
+            desc
+            role
+          }
+          urls {
+            backend
+            deployed
+            frontend
+          }
+        }
+      }
+    }
+  `)
 
+  return (
+    <Carousel interval={50000}>
+      {data.projects.nodes.map(proj => {
+        console.log(`tech >> `, proj.tech)
+        return (
+          <Carousel.Item key={proj.id}>
+            <Image
+              fluid={proj.image.url.childImageSharp.fluid}
+              alt={`Screenshot of ${proj.name}`}
+            />
+            <Carousel.Caption>
+              <Row className='d-flex align-items-center'>
+                <Col as='h4'>
+                  <a
+                    href={proj.urls.deployed}
+                    className='p-2 bg-primary border border-black rounded text-link'
+                  >
+                    {proj.name}{' '}
+                    <RiExternalLinkLine className='external-link-icon' />
+                  </a>
+                </Col>
+                {/* <Col
+                  as='h4'
+                  className='p-2 bg-primary border border-black rounded text-icon'
+                  >
+                  {proj.tech.map(icon => (
+                    <Icons key={icon} icons={{ icon }} />
+                    ))}
+                  </Col> */}
+                <Col as='h4'>
+                  <a
+                    href={proj.urls.frontend}
+                    className='p-2 bg-primary border border-black rounded text-link'
+                  >
+                    Repository{' '}
+                    <RiExternalLinkLine className='external-link-icon' />
+                  </a>
+                </Col>
+              </Row>
+            </Carousel.Caption>
+          </Carousel.Item>
+        )
+      })}
+    </Carousel>
+  )
+}
 export default Projects
